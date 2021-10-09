90dab69b463c51d16ca8bf269a9d6a20165cdbd7
Dimi <dkasderidis95@gmail.com>
2021-10-09 20:00:50 +0200

    Added header with empty classes

---
+++ expenses.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+
+
+//This classes should contain importance of bought product/or expens, amount of money the product cost, date the propdcut was bought
+//Each class is supposed to create a template for either a dynamic or a static expense.
+//Static expenses are supposed to stay in the DB while Dynamic Expenses are supposed to be whiped after a cycle (one week).
+class static_expenses 
+{
+
+    private:
+
+    public:
+
+    
+
+};
+
+class dynamic_expenses
+{
+    private:
+
+    public:
+
+};
